4442a1a5ca1a7a1df28a55f3c8776e162f15a87a
ramzi-bouzaiene <ramzibouzaiene00@gmail.com>
2026-03-17 21:36:43 +0100

    adding logger and DB config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import app from './app'
+import connectDB from './config/database';
 import { PORT } from "./config/env"
+
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
