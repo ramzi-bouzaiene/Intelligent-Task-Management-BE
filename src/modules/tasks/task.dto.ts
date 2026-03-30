1287765aa0303bd6616b53d47b99f8b3956af2d9
ramzi-bouzaiene <ramzibouzaiene00@gmail.com>
2026-03-30 11:46:40 +0100

    adding task model, migration and dto

---
+++ src/modules/tasks/task.dto.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+import { taskStatus } from '../../shared/constants/taskStatus';
+
+export const createTaskSchema = z.object({
+    title: z.string().min(1),
+    description: z.string().optional(),
+    status: z.enum([taskStatus.PENDING, taskStatus.IN_PROGRESS, taskStatus.COMPLETED]).default(taskStatus.PENDING),
+    user_id: z.number().optional(),
+    created_at: z.date().default(() => new Date()),
+
+})
+
+export const updateTaskSchema = z.object({
+    title: z.string().min(1).optional(),
+    description: z.string().optional(),
+    status: z.enum([taskStatus.PENDING, taskStatus.IN_PROGRESS, taskStatus.COMPLETED]).optional(),
+    updated_at: z.date().default(() => new Date()),
+})
+
+export const getTasksQuerySchema = z.object({
+    status: z.enum([taskStatus.PENDING, taskStatus.IN_PROGRESS, taskStatus.COMPLETED]).optional(),
+    title: z.string().optional(),
+})
+
+export type CreateTaskDto = z.infer<typeof createTaskSchema>;
+export type UpdateTaskDto = z.infer<typeof updateTaskSchema>;
+export type GetTasksQueryDto = z.infer<typeof getTasksQuerySchema>;
---
+++ src/shared/constants/taskStatus.ts
@@ -0,0 +1,5 @@
+export const taskStatus = {
+    PENDING: 'pending',
+    IN_PROGRESS: 'in_progress',
+    COMPLETED: 'completed',
+} as const;
