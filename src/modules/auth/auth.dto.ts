11e5045945190d5fda23a0d39b981696e17d3dc6
ramzi-bouzaiene <ramzibouzaiene00@gmail.com>
2026-06-24 10:23:54 +0100

    avatar to login response

--- src/modules/auth/auth.dto.ts
+++ src/modules/auth/auth.dto.ts
@@ -15,6 +15,7 @@ export const registerSchema = z.object({
 export const loginSchema = z.object({
   email: z.email(),
   password: z.string().min(6),
+  avatar: z.string().optional(),
 });
 
 export type RegisterDto = z.infer<typeof registerSchema>;
